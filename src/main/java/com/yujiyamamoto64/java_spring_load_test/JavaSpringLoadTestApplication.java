9d61fcdc183a0baad09e2ed2cae7fbf79f982f02
Yuji Yamamoto <yujiyamamoto64@gmail.com>
2025-11-14 17:38:12 -0300

    project created

---
+++ src/main/java/com/yujiyamamoto64/java_spring_load_test/JavaSpringLoadTestApplication.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.java_spring_load_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringLoadTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringLoadTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yujiyamamoto64/java_spring_load_test/JavaSpringLoadTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.java_spring_load_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringLoadTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
